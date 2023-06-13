d29214d9b994c60b5afc97f768f408ceace0fed8
Andrew Pethoud <andrewpethoud@gmail.com>
2023-06-13 14:34:35 -0700

    feat(Post): add timestamp

--- src/app/blog/[slug]/Post.jsx
+++ src/app/blog/[slug]/Post.jsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { H1, P } from "@/app/globalStyles";
+import Timestamp from "../Timestamp";
 
 export default function Post({ post }) {
   console.log("*** post is: ", post);
   return (
     <>
       <H1>{post.title}</H1>
+      <Timestamp date={post.created_at} />
       {post.paragraphs.map(paragraph => (
         <P>{paragraph.body}</P>
       ))}

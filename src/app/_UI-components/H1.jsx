292171eceee98e4252065cbc65bc5a555353a540
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-20 13:10:49 -0700

    refactor(H1): change h1 color to purple

--- src/app/_UI-components/H1.jsx
+++ src/app/_UI-components/H1.jsx
@@ -2,7 +2,7 @@ import { latoBold } from "../fonts"
 
 export default function H1({ children }) {
   return (
-    <div className={`${latoBold.className} text-3xl text-slate-900 dark:text-slate-100 my-4`}>
+    <div className={`${latoBold.className} text-3xl text-violet-600 dark:text-violet-400 my-4`}>
       {children}
     </div>
   )

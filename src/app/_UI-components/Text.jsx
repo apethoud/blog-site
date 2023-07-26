83c41d31e2a0427ce23b075113dec3bcb6b3c077
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-26 15:13:35 -0700

    fix(Post): reduce line length on large screen sizes

--- src/app/_UI-components/Text.jsx
+++ src/app/_UI-components/Text.jsx
@@ -1,7 +1,6 @@
 import { lato, latoItalic } from "../fonts"
 
 export default function Text({ children, italic }) {
-  console.log("italic is: ", italic)
   return (
     <div className={`${italic ? latoItalic.className : lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-4`}>
       {children}
--- src/app/blog/[slug]/Post.jsx
+++ src/app/blog/[slug]/Post.jsx
@@ -5,7 +5,6 @@ import Text from "@/app/_UI-components/Text";
 import { formatDate } from "@/app/utils";
 
 export default function Post({ post }) {
-  console.log("*** post is: ", post);
   return (
     <>
       <H1>{post.title}</H1>

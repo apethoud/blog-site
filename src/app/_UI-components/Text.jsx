44e7750c3be7e9ac43caaaf8efd166994fb69502
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-26 14:45:38 -0700

    fix(Text): add italic support

--- src/app/_UI-components/Text.jsx
+++ src/app/_UI-components/Text.jsx
@@ -1,8 +1,9 @@
-import { lato } from "../fonts"
+import { lato, latoItalic } from "../fonts"
 
 export default function Text({ children, italic }) {
+  console.log("italic is: ", italic)
   return (
-    <div className={`${lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-4 ${italic ? "italic" : ""}`}>
+    <div className={`${italic ? latoItalic.className : lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-4`}>
       {children}
     </div>
   )

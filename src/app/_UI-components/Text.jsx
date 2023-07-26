581e43924ff955e644652987a78872342aa278ff
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-26 14:15:35 -0700

    refactor(Text): increase font size

--- src/app/_UI-components/Text.jsx
+++ src/app/_UI-components/Text.jsx
@@ -2,7 +2,7 @@ import { lato } from "../fonts"
 
 export default function Text({ children, italic }) {
   return (
-    <div className={`${lato.className} text-lg text-slate-900 dark:text-slate-100 antialiased my-4 ${italic ? "italic" : ""}`}>
+    <div className={`${lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-4 ${italic ? "italic" : ""}`}>
       {children}
     </div>
   )

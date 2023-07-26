95f560eaa22a7e95068850864d973bef73382f6b
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-26 15:15:10 -0700

    fix(Text): add more vertical space between paragraphs

--- src/app/_UI-components/Text.jsx
+++ src/app/_UI-components/Text.jsx
@@ -2,7 +2,7 @@ import { lato, latoItalic } from "../fonts"
 
 export default function Text({ children, italic }) {
   return (
-    <div className={`${italic ? latoItalic.className : lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-4`}>
+    <div className={`${italic ? latoItalic.className : lato.className} text-xl text-slate-900 dark:text-slate-100 antialiased my-6`}>
       {children}
     </div>
   )

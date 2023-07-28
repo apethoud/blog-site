d09dd02d1090dff6c6e47a9bf7e038b4dbba1f1c
Andrew Pethoud <andrewpethoud@gmail.com>
2023-07-28 11:19:46 -0700

    chore(Headers): remove obsolete header component files

--- src/app/_UI-components/H3.jsx
+++
@@ -1,9 +0,0 @@
-import { latoBold } from "../fonts"
-
-export default function H3({ children }) {
-  return (
-    <div className={`${latoBold.className} text-xl text-slate-900 dark:text-slate-100 my-4`}>
-      {children}
-    </div>
-  )
-}
